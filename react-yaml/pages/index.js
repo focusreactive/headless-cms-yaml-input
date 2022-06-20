2ffffb5570f066f4abecc99cbb47f17ab997f43b
oleg proskurin <regx@usul.su>
2022-06-20 18:18:12 +0700

    Add more examples

--- react-yaml/pages/index.js
+++ react-yaml/pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { oneDark } from '@codemirror/theme-one-dark';
 import YamlEditor from '../src';
 
@@ -23,14 +24,74 @@ const obj = {
   newLine: 'blasdf\nblaslsls\n',
 };
 
+const FollowByText = () => {
+  const [textValue, setTextValue] = React.useState('');
+
+  const handleChange = ({ json, text }) => {
+    setTextValue(text);
+  };
+
+  return (
+    <>
+      <h1>Following mode</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ width: '30%' }}>
+          <h2>Source object</h2>
+          <YamlEditor json={obj} onChange={handleChange} theme={oneDark} />
+        </div>
+        <div style={{ width: '30%' }}>
+          <h2>Current value</h2>
+          <textarea style={{ width: '100%', height: 200 }} value={textValue} />
+        </div>
+        <div style={{ width: '30%' }}>
+          <h2>Following editor</h2>
+          <YamlEditor text={textValue} theme={oneDark} key={textValue} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const FollowByObj = () => {
+  const [jsonValue, setJsonValue] = React.useState(null);
+
+  const handleChange = ({ json, text }) => {
+    setJsonValue(json);
+  };
+
+  return (
+    <>
+      <h1>Following mode</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ width: '30%' }}>
+          <h2>Source object</h2>
+          <YamlEditor json={obj} onChange={handleChange} theme={oneDark} />
+        </div>
+        <div style={{ width: '30%' }}>
+          <h2>Current value</h2>
+          <textarea
+            style={{ width: '100%', height: 200 }}
+            value={JSON.stringify(jsonValue)}
+          />
+        </div>
+        <div style={{ width: '30%' }}>
+          <h2>Following editor</h2>
+          <YamlEditor
+            json={jsonValue}
+            theme={oneDark}
+            key={JSON.stringify(jsonValue)}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default function Home() {
   return (
     <div>
-      <YamlEditor
-        json={obj}
-        onChange={(value) => console.log('on change: ', value)}
-        theme={oneDark}
-      />
+      <FollowByText />
+      <FollowByObj />
     </div>
   );
 }
